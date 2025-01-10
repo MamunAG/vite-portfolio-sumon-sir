7047d37f76cdfd25ff05a58f38de67ed2bc43a54
ubungtu <mamun166036@gmail.com>
2025-01-10 07:27:04 +0600

    .env up

--- src/actions/api.ts
+++ src/actions/api.ts
@@ -1,2 +1,4 @@
-export const apiRoot = "https://ssadmin-blond.vercel.app/api";
+// export const apiRoot = "https://ssadmin-blond.vercel.app/api";
 // export const apiRoot = "http://localhost:3000/api";
+
+export const apiRoot = import.meta.env.VITE_API_ROOT;

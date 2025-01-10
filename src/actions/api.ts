e00ab80a18c7c226774d78f951a92eabafd45580
ubungtu <mamun166036@gmail.com>
2025-01-10 07:11:15 +0600

    api

--- src/actions/api.ts
+++ src/actions/api.ts
@@ -1,2 +1,2 @@
-// export const apiRoot = "https://ssadmin-blond.vercel.app/api";
-export const apiRoot = "http://localhost:3000/api";
+export const apiRoot = "https://ssadmin-blond.vercel.app/api";
+// export const apiRoot = "http://localhost:3000/api";
